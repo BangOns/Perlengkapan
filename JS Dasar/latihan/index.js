1f2654fee8204146f0cb3df37b99b6198443fbb3
BangOns <97930260+BangOns@users.noreply.github.com>
2022-05-25 14:22:27 +0700

    Belajar JS

---
+++ JS Dasar/latihan/index.js
@@ -0,0 +1,36 @@
+var penumpang = [];
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    penumpang.push(namaPenumpang);
+    return penumpang;
+  } else {
+    for (var i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == undefined) {
+        penumpang[i] = namaPenumpang;
+        return penumpang;
+      } else if (penumpang[i] == namaPenumpang) {
+        console.log("penumpang sudah masuk");
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        penumpang.push(namaPenumpang);
+        return penumpang;
+      }
+    }
+  }
+};
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    console.log("Belum ada penumpang");
+    return penumpang;
+  } else {
+    for (var i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[i] = undefined;
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        console.log("nama penumpang tidak ada");
+        return penumpang;
+      }
+    }
+  }
+};
